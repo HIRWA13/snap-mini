4fc8547a50254db8b34a1d0e7e7e7ee8c3717556
Junior Hirwa <iamhirwejr@gmail.com>
2023-10-21 20:34:27 +0200

    style: update the appearance of desktop version

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,28 +1,32 @@
 import { ReactElement } from "react"
 import mobileHero from "/mobileHero.png";
 import Clients from "./Clients";
+import heroImage from "../assets/imageherodesktop.png"
 
 export default function Main():ReactElement {
   return (
-    <main className="space-y-8">
-      <section>
-        <img src={mobileHero} />
+    <main className="space-y-8 md:p-3 md:flex md:items-center md:justify-between md:w-4/5 md:mx-auto">
+      <section className=" md:w-1/2 md:space-y-12">
+        <section>
+          <img src={mobileHero} className="md:hidden" />
+        </section>
+        <section className="p-3 mt-5 md:mt-0 flex flex-col items-center justify-center gap-3 md:items-start md:space-y-8">
+          <h1 className="text-4xl text-dark text-center font-Epilogue font-bold md:text-8xl md:text-left">
+            Make remote work
+          </h1>
+          <p className="px-2 text-gray font-Epilogue text-center md:text-left md:text-2xl">
+            Get your team in sync, no matter your location. Streamline
+            processes, create team rituals, and watch productivity soar.
+          </p>
+          <button className="bg-dark text-light py-3 px-7 rounded-xl font-Epilogue font-bold mt-2">
+            Learn more
+          </button>
+        </section>
+        <section className="px-4">
+          <Clients />
+        </section>
       </section>
-      <section className="p-3 mt-5 flex flex-col items-center justify-center gap-3">
-        <h1 className="text-4xl text-dark text-center font-Epilogue font-bold">
-          Make remote work
-        </h1>
-        <p className="px-2 text-gray font-Epilogue text-center">
-          Get your team in sync, no matter your location. Streamline processes,
-          create team rituals, and watch productivity soar.
-        </p>
-        <button className="bg-dark text-light py-3 px-4 rounded-xl font-Epilogue font-bold mt-2">
-          Learn more
-        </button>
-      </section>
-      <section className="px-4">
-        <Clients />
-      </section>
+      <img src={heroImage} className="hidden md:block md:w-1/3" />
     </main>
   );
 }
