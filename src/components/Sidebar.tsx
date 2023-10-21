d59c91bb262af49917145e5d74fd6440137942e6
Junior Hirwa <iamhirwejr@gmail.com>
2023-10-21 19:58:40 +0200

    style: update the styles of the side bar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -9,10 +9,19 @@ type SidebarProps = {
 export default function Sidebar({isOpen, setIsOpen}: SidebarProps): ReactElement {
   return (
     <section className="text-center flex p-4 w-full absolute top-0 right-0 h-screen">
-      <div className="bg-dark h-full w-full flex-1 fixed top-0 left-0 opacity-80">.</div>
-      <div  className="w-4/6 fixed bg-light opacity-100 top-0 right-0 h-full">
+      <div className="bg-dark h-full w-full flex-1 fixed top-0 left-0 opacity-80">
+        .
+      </div>
+      <div className="w-4/6 fixed flex-col bg-light opacity-100 top-0 right-0 h-full">
         <div className="p-2">
-          <img src={close} className="fixed top-5 right-4 cursor-pointer" onClick={()=> setIsOpen(!isOpen)}/>
+          <img
+            src={close}
+            className="fixed top-5 right-4 cursor-pointer"
+            onClick={() => setIsOpen(!isOpen)}
+          />
+        </div>
+        <div className="mt-14 w-full h-full">
+          mobile content here
         </div>
       </div>
     </section>
