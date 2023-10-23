b559adda764c138d0f6b58a81bf113135251d91c
Junior Hirwa <iamhirwejr@gmail.com>
2023-10-23 07:47:57 +0200

    style: update the styles in the main component

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -6,15 +6,15 @@ import heroImage from "../assets/imageherodesktop.png"
 export default function Main():ReactElement {
   return (
     <main className="space-y-8 md:p-3 md:flex md:items-center md:justify-around md:w-4/5 md:mx-auto">
-      <section className=" md:w-1/2 md:space-y-12">
+      <section className=" md:w-1/2 md:space-y-24">
         <section>
           <img src={mobileHero} className="md:hidden" />
         </section>
-        <section className="p-3 mt-5 md:mt-0 flex flex-col items-center justify-center gap-3 md:items-start md:space-y-8 md:p-0">
-          <h1 className="text-4xl text-dark text-center font-Epilogue font-bold md:text-8xl md:pr-4 md:text-left">
+        <section className="mt-5 md:mt-0 flex flex-col items-center justify-center gap-3 md:items-start md:space-y-6 md:p-0">
+          <h1 className="text-4xl text-dark text-center font-Epilogue font-black md:text-7xl md:text-left md:pr-32">
             Make remote work
           </h1>
-          <p className="px-3 text-lg text-gray font-Epilogue text-center md:text-left md:text-2xl md:px-0 md:pr-20">
+          <p className="text-gray font-Epilogue text-center md:text-left md:text-xl md:px-0 md:pr-20">
             Get your team in sync, no matter your location. Streamline
             processes, create team rituals, and watch productivity soar.
           </p>
@@ -22,7 +22,7 @@ export default function Main():ReactElement {
             Learn more
           </button>
         </section>
-        <section className="px-4">
+        <section className="px-4 mt-3 md:mt-0 md:p-0">
           <Clients />
         </section>
       </section>
