8404ddcc96934fd03f60a3a79f7254cbb488480c
Junior Hirwa <iamhirwejr@gmail.com>
2023-10-24 07:47:00 +0200

    style: update styles

--- src/components/Feature.tsx
+++ src/components/Feature.tsx
@@ -1,4 +1,4 @@
-import { ReactElement } from "react";
+import { ReactElement, useState } from "react";
 import { Menu } from "@headlessui/react";
 import planning from "../assets/iconplanning.svg"
 import todo from "../assets/icontodo.svg"
@@ -6,23 +6,24 @@ import calendar from "../assets/iconcalendar.svg"
 import reminders from "../assets/iconreminders.svg"
 
 export default function Feature(): ReactElement {
+  const [isArrowOpen, setIsArrowOpen] = useState(false)
   return (
     <Menu as="div">
-      <Menu.Button className="font-Epilogue text-xl">Feature</Menu.Button>
-      <Menu.Items className="absolute bg-light px-2 w-36 shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
-        <div className="py-2 flex items-center space-x-5">
+      <Menu.Button className="font-Epilogue text-xl" onClick={()=> {console.log("clicked")}}>Feature</Menu.Button>
+      <Menu.Items className="absolute w-40 bg-light rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
+        <div className="flex items-center space-x-5 w-full p-2">
           <img src={todo} />
           <h1>Todo List</h1>
         </div>
-        <div className="py-2 flex items-center space-x-5">
+        <div className="flex items-center space-x-5 w-full p-2">
           <img src={calendar} />
           <h1>Calendar</h1>
         </div>
-        <div className="py-2 flex items-center space-x-5">
+        <div className="flex items-center space-x-5 w-full p-2">
           <img src={reminders} />
           <h1>Remiders</h1>
         </div>
-        <div className="py-2 flex items-center space-x-5">
+        <div className="flex items-center space-x-5 w-full p-2">
           <img src={planning} />
           <h1>Planning</h1>
         </div>
