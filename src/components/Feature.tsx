5ab3ceeedf6b43a04a4d4c993eac6ad99bb2d53d
Junior Hirwa <iamhirwejr@gmail.com>
2023-10-24 08:01:29 +0200

    feat: update the structure and style of the feature component

--- src/components/Feature.tsx
+++ src/components/Feature.tsx
@@ -9,24 +9,33 @@ export default function Feature(): ReactElement {
   const [isArrowOpen, setIsArrowOpen] = useState(false)
   return (
     <Menu as="div">
-      <Menu.Button className="font-Epilogue text-xl" onClick={()=> {console.log("clicked")}}>Feature</Menu.Button>
+      <Menu.Button as="li"
+        className="font-Epilogue cursor-pointer"
+        onClick={() => {
+          console.log("clicked");
+        }}
+      >
+        Feature
+      </Menu.Button>
       <Menu.Items className="absolute left-20 w-40 bg-light rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
-        <div className="flex items-center space-x-5 w-full p-2">
+       <div className="flex flex-col items-center justify-center">
+         <div className="flex items-center space-x-5 w-full p-2">
           <img src={todo} />
-          <h1>Todo List</h1>
+          <h1 className="font-Epilogue text-base">Todo List</h1>
         </div>
-        <div className="flex items-center space-x-5 w-full p-2">
+        <div className="flex items-center space-x-4 w-full p-2">
           <img src={calendar} />
-          <h1>Calendar</h1>
+          <h1 className="font-Epilogue text-base">Calendar</h1>
         </div>
         <div className="flex items-center space-x-5 w-full p-2">
           <img src={reminders} />
-          <h1>Remiders</h1>
+          <h1 className="font-Epilogue text-base">Remiders</h1>
         </div>
-        <div className="flex items-center space-x-5 w-full p-2">
+        <div className="flex items-center space-x-4 w-full p-2">
           <img src={planning} />
-          <h1>Planning</h1>
+          <h1 className="font-Epilogue text-base">Planning</h1>
         </div>
+       </div>
       </Menu.Items>
     </Menu>
   );
