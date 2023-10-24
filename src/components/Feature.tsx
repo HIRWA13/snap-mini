3809bc18ef42fa5b5c8e289623f05cf915d70926
Junior Hirwa <iamhirwejr@gmail.com>
2023-10-24 08:18:05 +0200

    style: update the style of the feature component

--- src/components/Feature.tsx
+++ src/components/Feature.tsx
@@ -9,7 +9,8 @@ export default function Feature(): ReactElement {
   const [isArrowOpen, setIsArrowOpen] = useState(false)
   return (
     <Menu as="div">
-      <Menu.Button as="li"
+      <Menu.Button
+        as="li"
         className="font-Epilogue cursor-pointer"
         onClick={() => {
           console.log("clicked");
@@ -17,25 +18,25 @@ export default function Feature(): ReactElement {
       >
         Feature
       </Menu.Button>
-      <Menu.Items className="absolute left-20 w-40 bg-light rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
-       <div className="flex flex-col items-center justify-center">
-         <div className="flex items-center space-x-5 w-full p-2">
-          <img src={todo} />
-          <h1 className="font-Epilogue text-base">Todo List</h1>
+      <Menu.Items className="absolute left-20 top-14 w-40 bg-light rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
+        <div className="flex flex-col items-center justify-center">
+          <div className="flex items-center space-x-5 w-full p-2 px-5">
+            <img src={todo} />
+            <h1 className="font-Epilogue text-base text-gray">Todo List</h1>
+          </div>
+          <div className="flex items-center space-x-4 w-full p-2 px-5">
+            <img src={calendar} />
+            <h1 className="font-Epilogue text-base text-gray">Calendar</h1>
+          </div>
+          <div className="flex items-center space-x-5 w-full p-2 px-5">
+            <img src={reminders} />
+            <h1 className="font-Epilogue text-base text-gray">Remiders</h1>
+          </div>
+          <div className="flex items-center space-x-4 w-full p-2 px-5">
+            <img src={planning} />
+            <h1 className="font-Epilogue text-base text-gray">Planning</h1>
+          </div>
         </div>
-        <div className="flex items-center space-x-4 w-full p-2">
-          <img src={calendar} />
-          <h1 className="font-Epilogue text-base">Calendar</h1>
-        </div>
-        <div className="flex items-center space-x-5 w-full p-2">
-          <img src={reminders} />
-          <h1 className="font-Epilogue text-base">Remiders</h1>
-        </div>
-        <div className="flex items-center space-x-4 w-full p-2">
-          <img src={planning} />
-          <h1 className="font-Epilogue text-base">Planning</h1>
-        </div>
-       </div>
       </Menu.Items>
     </Menu>
   );
