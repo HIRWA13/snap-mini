b6e47ae4835ad6fe7057df601ef9a9800db52f0c
Junior Hirwa <iamhirwejr@gmail.com>
2023-10-24 07:47:34 +0200

    style: update navigation styles

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,10 +1,12 @@
 import { ReactElement } from "react";
 import Feature from "./Feature";
+import Company from "./Company";
 
 export default function Navigation(): ReactElement {
   return (
-    <ul className="hidden md:block">
+    <ul className="hidden md:flex items-start gap-x-14">
         <Feature/>
+        <Company/>
     </ul>
   )
 }
