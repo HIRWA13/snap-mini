1e29bd4d10168bc65338901803b36fa5fea0d385
Junior Hirwa <iamhirwejr@gmail.com>
2023-10-24 08:47:00 +0200

    feat: introduce a state to handle arrows

--- src/components/Company.tsx
+++ src/components/Company.tsx
@@ -1,19 +1,27 @@
 import { ReactElement } from "react";
 import { Menu } from "@headlessui/react";
+import arrowUp from "../assets/iconarrowup.svg";
+import arrowDown from "../assets/iconarrowdown.svg";
 
-export default function Company(): ReactElement {
+interface CompanyProps {
+  isArrowOpen: boolean;
+  arrowHandler: () => void;
+}
+
+export default function Company({isArrowOpen, arrowHandler}: CompanyProps): ReactElement {
   return (
     <Menu>
       <Menu as="div">
-        <Menu.Button
-          as="li"
-          className="font-Epilogue cursor-pointer hover:text-gray"
-          onClick={() => {
-            console.log("clicked");
-          }}
-        >
-          Company
-        </Menu.Button>
+        <div className="flex items-center gap-x-3 cursor-pointer">
+          <Menu.Button
+            as="li"
+            className="font-Epilogue cursor-pointer hover:text-gray"
+            onClick={arrowHandler}
+          >
+            Company
+          </Menu.Button>
+          {isArrowOpen ? <img src={arrowDown} /> : <img src={arrowUp} />}
+        </div>
         <Menu.Items className="absolute top-14 w-32 py-2 bg-light rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
           <div className="flex items-center space-x-5 w-full p-2 px-4">
             <h1 className="font-Epilogue text-base text-gray">History</h1>
