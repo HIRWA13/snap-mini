54c1b43298e859629aeda46a8f29e7cc5466998e
Junior Hirwa <iamhirwejr@gmail.com>
2023-10-24 08:11:50 +0200

    style: update company component styles

--- src/components/Company.tsx
+++ src/components/Company.tsx
@@ -6,22 +6,23 @@ export default function Company(): ReactElement {
     <Menu>
       <Menu as="div">
         <Menu.Button
-          className="font-Epilogue text-xl"
+          as="li"
+          className="font-Epilogue cursor-pointer"
           onClick={() => {
             console.log("clicked");
           }}
         >
           Company
         </Menu.Button>
-        <Menu.Items className="absolute w-32 bg-light rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
-          <div className="flex items-center space-x-5 w-full p-2">
-            <h1>History</h1>
+        <Menu.Items className="absolute top-14 w-32 py-2 bg-light rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
+          <div className="flex items-center space-x-5 w-full p-2 px-4">
+            <h1 className="font-Epilogue text-base text-gray">History</h1>
           </div>
-          <div className="flex items-center space-x-5 w-full p-2">
-            <h1>Our Team</h1>
+          <div className="flex items-center space-x-5 w-full p-1 px-4">
+            <h1 className="font-Epilogue text-base text-gray">Our Team</h1>
           </div>
-          <div className="flex items-center space-x-5 w-full p-2">
-            <h1>Blog</h1>
+          <div className="flex items-center space-x-5 w-full p-2 px-4">
+            <h1 className="font-Epilogue text-base text-gray">Blog</h1>
           </div>
         </Menu.Items>
       </Menu>
