c3959a46a50724e18b7f8b4f8bacb6aea76b0330
Junior Hirwa <iamhirwejr@gmail.com>
2023-10-25 07:56:06 +0200

    style: update the styles of the login and signup

--- src/components/LoginSignUp.tsx
+++ src/components/LoginSignUp.tsx
@@ -4,7 +4,7 @@ export default function LoginSignUp():ReactElement {
   return (
     <div className="space-x-4 hidden lg:block">
         <button className="font-Epilogue font-medium cursor-pointer">Login</button>
-        <button className="py-2 px-7 border-2 rounded-xl font-Epilogue font-medium">Register</button>
+        <button className="py-2 px-7 border-2 rounded-xl font-Epilogue font-medium hover:bg-dark hover:text-light transition-all duration-200">Register</button>
     </div>
   )
 }
