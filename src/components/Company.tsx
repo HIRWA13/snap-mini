f5d5ad623ea7f45608e70450c79ec38c4355b9ef
Junior Hirwa <iamhirwejr@gmail.com>
2023-10-25 07:34:58 +0200

    feat: create a state to control the arrow

--- src/components/Company.tsx
+++ src/components/Company.tsx
@@ -1,14 +1,14 @@
-import { ReactElement } from "react";
+import { ReactElement, useState } from "react";
 import { Menu } from "@headlessui/react";
 import arrowUp from "../assets/iconarrowup.svg";
 import arrowDown from "../assets/iconarrowdown.svg";
 
-interface CompanyProps {
-  isArrowOpen: boolean;
-  arrowHandler: () => void;
-}
 
-export default function Company({isArrowOpen, arrowHandler}: CompanyProps): ReactElement {
+export default function Company(): ReactElement {
+  const [isCompanyArrowDown, setIsCompanyArrowDown] = useState(true);
+  const companyArrowHandler = () => {
+    setIsCompanyArrowDown(!isCompanyArrowDown);
+  }
   return (
     <Menu>
       <Menu as="div">
@@ -16,11 +16,11 @@ export default function Company({isArrowOpen, arrowHandler}: CompanyProps): Reac
           <Menu.Button
             as="li"
             className="font-Epilogue cursor-pointer hover:text-gray"
-            onClick={arrowHandler}
+            onClick={companyArrowHandler}
           >
             Company
           </Menu.Button>
-          {isArrowOpen ? <img src={arrowDown} /> : <img src={arrowUp} />}
+          {isCompanyArrowDown ? <img src={arrowDown} /> : <img src={arrowUp} />}
         </div>
         <Menu.Items className="absolute top-14 w-32 py-2 bg-light rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
           <div className="flex items-center space-x-5 w-full p-2 px-4">
