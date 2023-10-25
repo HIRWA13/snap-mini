39718be5ae7f675f3f7c5e69afebb374c52f4338
Junior Hirwa <iamhirwejr@gmail.com>
2023-10-25 07:55:34 +0200

    feat: remove the state

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,19 +1,16 @@
-import { ReactElement, useState } from "react";
+import { ReactElement} from "react";
 import Feature from "./Feature";
 import Company from "./Company";
 
 export default function Navigation(): ReactElement {
-    const [isArrowOpen, setIsArrowOpen] = useState(false);
-    const arrowHandler = () => {
-        setIsArrowOpen(!isArrowOpen);
-    }
+
   return (
     <ul className="hidden md:flex items-start gap-x-14">
+        <li>
+          <Feature />
+        </li>
       <li>
-        <Feature arrowHandler={arrowHandler} isArrowOpen={isArrowOpen} />
-      </li>
-      <li>
-        <Company arrowHandler={arrowHandler} isArrowOpen={isArrowOpen}/>
+        <Company />
       </li>
       <li className="font-Epilogue cursor-pointer hover:text-gray">Careers</li>
       <li className="font-Epilogue cursor-pointer hover:text-gray">About</li>
