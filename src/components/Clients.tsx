56ce0862e28e73bb49d9394cd3e2a9f82623fbd6
Junior Hirwa <iamhirwejr@gmail.com>
2023-10-27 09:17:55 +0200

    fix: icons file path

--- src/components/Clients.tsx
+++ src/components/Clients.tsx
@@ -1,16 +1,12 @@
 import { ReactElement } from "react";
-import clientaudiophile from "/src/assets/clientaudiophile.svg";
-import clientdatabiz from "/src/assets/clientdatabiz.svg";
-import clientmaker from "/src/assets/clientmaker.svg";
-import clientmeet from "/src/assets/clientmeet.svg";
 
 export default function Clients(): ReactElement {
   return (
     <section className="flex items-center justify-between gap-3 md:justify-start md:gap-x-10">
-      <img className="h-5" src={clientdatabiz}/>
-      <img className="h-5" src={clientaudiophile}/>
-      <img className="h-5" src={clientmeet}/>
-      <img className="h-5" src={clientmaker}/>
+      <img className="h-5" src="/src/assets/clientdatabiz.svg" />
+      <img className="h-5" src="/src/assets/clientaudiophile.svg" />
+      <img className="h-5" src="/src/assets/clientmaker.svg" />
+      <img className="h-5" src="/src/assets/clientmeet.svg" />
     </section>
   );
 }
