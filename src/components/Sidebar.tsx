54199b001b413b58d334e0aaf90e3780a20f02ad
Junior Hirwa <iamhirwejr@gmail.com>
2023-10-27 08:42:43 +0200

    feat: update sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -9,8 +9,11 @@ type SidebarProps = {
 
 export default function Sidebar({isOpen, setIsOpen}: SidebarProps): ReactElement {
   return (
-    <section className="text-center flex p-4 w-full absolute top-0 right-0 h-screen">
-      <div className="bg-dark h-full w-full flex-1 fixed top-0 left-0 opacity-80">
+    <section className="md:hidden text-center flex p-4 w-full absolute top-0 right-0 h-screen">
+      <div
+        className="bg-dark h-full w-full flex-1 fixed top-0 left-0 opacity-80"
+        onClick={() => setIsOpen(!isOpen)}
+      >
         .
       </div>
       <div className="w-4/6 fixed flex-col bg-light opacity-100 top-0 right-0 h-full">
@@ -23,7 +26,6 @@ export default function Sidebar({isOpen, setIsOpen}: SidebarProps): ReactElement
         </div>
         <div className="mt-14 w-full h-full">
           <FeatureMobile />
-          mobile
         </div>
       </div>
     </section>
